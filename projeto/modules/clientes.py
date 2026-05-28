1a166ac8dde3251cc6c1550109fadd07de827551
alissoncunha06 <158228524+alissoncunha06@users.noreply.github.com>
2026-05-28 11:53:51 -0300

    Update clientes.py

--- projeto/modules/clientes.py
+++ projeto/modules/clientes.py
@@ -3,12 +3,6 @@ modulo de clientes: cadastrar, listar, editar e remover os clientes da loja
 id, nome, cpf, telefone, email
 """
 
-def show_menu():
-    #usar flask?
-    pass
-
-def load_data():
-    pass
-
-def save_data():
-    pass
+import pickle
+import os
+ARQUIVO = "clientes.pkl"
